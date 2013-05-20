87c5c8bf0ffd5a19f975f536f53402e1518fd265
raincole <raincolee@gmail.com>
2013-05-21 03:01:07 +0800

    Add personal FileUtil

---
+++ src/main/scala/cc/raintomorrow/FileUtils.scala
@@ -0,0 +1,8 @@
+package cc.raintomorrow
+
+object FileUtils {
+  def printToFile(f: java.io.File)(op: java.io.PrintWriter => Unit) {
+    val p = new java.io.PrintWriter(f)
+    try { op(p) } finally { p.close() }
+  }
+}

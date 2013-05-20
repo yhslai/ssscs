cdf435211d7604f1520d659ec631029720abe961
raincole <raincolee@gmail.com>
2013-05-20 21:35:07 +0800

    Small refactor, typo, etc

---
+++ src/main/scala/SssCralwer.scala
@@ -0,0 +1,87 @@
+import scala.collection.JavaConversions._
+import java.text.SimpleDateFormat
+import java.util.Date
+import org.jsoup.Jsoup
+import org.jsoup.nodes._
+
+case class CrawlingConfig(itemCount: Int,
+                          since: Option[Date] = None,
+                          until: Option[Date] = None,
+                          onlyNewItem: Boolean = false)
+
+class SssCralwer(config: CrawlingConfig) {
+
+  def crawlInfos(config: CrawlingConfig): Vector[ArticleInfo] = {
+    val mainListUrl = "http://www.scientificamerican.com/view/utils/Archive.cfc"
+    val contentTypeId = "25"
+    val farFuture = new SimpleDateFormat("yyyy-MM-dd").parse("2020-01-01")
+    val endDate = config.until.getOrElse(farFuture)
+    val onceItemCount = 100
+    val maxIterationCount = 100
+
+    def crawlIter(startRow: Int):
+        Vector[ArticleInfo] = {
+
+      val dateFormat = new SimpleDateFormat("M/dd/yy hh:mm:ss")
+      val connect = Jsoup.connect(mainListUrl)
+        .data("method", "loadContentScroll")
+        .data("maxRows", onceItemCount.toString)
+        .data("contenttype_id", contentTypeId)
+        .data("endDate", dateFormat.format(endDate))
+        .data("startRow", startRow.toString)
+      val doc = connect.get()
+
+      getInfosFromDoc(doc)
+    }
+
+    def filterEffective(infos: Vector[ArticleInfo]): Vector[ArticleInfo] = {
+      val effectiveInfos = config.since match {
+        case None => config.until match {
+          case None => infos
+          case Some(date) => infos.filter(info => info.date.before(date))
+        }
+        case Some(date) => infos.filter(info => info.date.after(date))
+      }
+
+      effectiveInfos
+    }
+
+    def isEnough(infos: Vector[ArticleInfo]): Boolean = {
+      infos.size >= config.itemCount
+    }
+
+    val iterStarter = Iterator.iterate(Vector.empty[ArticleInfo]) _
+    val iterator = iterStarter(infos => {
+      filterEffective(infos ++ crawlIter(infos.size + 1))
+    })
+    val results = iterator.take(maxIterationCount).toStream
+    val result = results.find(vi => isEnough(vi))
+
+    result match {
+      case None => {
+        val bestResult = results.last
+        println(s"Crawling failed! Can only crawl ${bestResult.size} items.")
+        bestResult
+      }
+      case Some(infos) => infos
+    }
+  }
+
+  private def getInfosFromDoc(doc: Document): Vector[ArticleInfo] = {
+    def getInfoFromListItem(li: Element): ArticleInfo = {
+      val header = li.select("h3").first
+      val url = header.select("a").attr("href")
+      val title = header.text()
+
+      val dateStr = li.select(".datestamp").text
+      val dateFormat = new java.text.SimpleDateFormat("MMM d, yyyy")
+      val date = dateFormat.parse(dateStr)
+
+      ArticleInfo(url, title, date)
+    }
+
+    val list = doc.select(".hasThumb.message_box")
+    list.map(l => getInfoFromListItem(l)).toVector
+  }
+
+}
--- src/main/scala/Ssscs.scala
+++ src/main/scala/Ssscs.scala
@@ -1,99 +1,13 @@
-import java.text.SimpleDateFormat
-import java.util.Date
-import org.jsoup.Jsoup
-import org.jsoup.nodes.{Document, Element}
-import org.jsoup.select.Elements
-import scala.collection.JavaConversions._
 import sext._
 
 object Ssscs {
-  case class CrawlConfig(
-                          itemCount: Int,
-                          since: Option[Date] = None,
-                          until: Option[Date] = None,
-                          onlyNewItem: Boolean = false
-                        )
-
 
   def main(args: Array[String]) {
-    val defaultConfig = CrawlConfig(200)
-    val infos = crawlInfos(defaultConfig)
+    val defaultConfig = CrawlingConfig(200)
+    val cralwer = new SssCralwer(defaultConfig)
+    val infos = cralwer.crawlInfos(defaultConfig)
 
     println(infos.valueTreeString)
-  }
-
-  private def crawlInfos(config: CrawlConfig): Vector[ArticleInfo] = {
-    val mainListUrl = "http://www.scientificamerican.com/view/utils/Archive.cfc"
-    val contentTypeId = "25"
-    val farFuture = new SimpleDateFormat("yyyy-MM-dd").parse("2020-01-01")
-    val endDate = config.until.getOrElse(farFuture)
-    val onceItemCount = 100
-    val maxIterationCount = 100
-
-    def crawlIter(startRow: Int):
-        Vector[ArticleInfo] = {
-
-      val dateFormat = new SimpleDateFormat("M/dd/yy hh:mm:ss")
-      val connect = Jsoup.connect(mainListUrl)
-                     .data("method", "loadContentScroll")
-                     .data("maxRows", onceItemCount.toString)
-                     .data("contenttype_id", contentTypeId)
-                     .data("endDate", dateFormat.format(endDate))
-                     .data("startRow", startRow.toString)
-      println(connect)
-      val doc = connect.get()
-
-      getInfosFromDoc(doc)
-    }
-
-    def filterEffectiv(infos: Vector[ArticleInfo]): Vector[ArticleInfo] = {
-      val effectiveInfos = config.since match {
-        case None => config.until match {
-          case None => infos
-          case Some(date) => infos.filter(info => info.date.before(date))
-        }
-        case Some(date) => infos.filter(info => info.date.after(date))
-      }
-
-      effectiveInfos
-    }
-
-    def isEnough(infos: Vector[ArticleInfo]): Boolean = {
-      infos.size >= config.itemCount
-    }
-
-    val iterStarter = Iterator.iterate(Vector.empty[ArticleInfo]) _
-    val iterator = iterStarter(infos => {
-      filterEffectiv(infos ++ crawlIter(infos.size + 1))
-    })
-    val results = iterator.take(maxIterationCount).toStream
-    val result = results.find(vi => isEnough(vi))
-
-    result match {
-      case None => {
-        val bestResult = results.last
-        println(s"Crawling failed! Can only crawl ${bestResult.size} items.")
-        bestResult
-      }
-      case Some(infos) => infos
-    }
-  }
-
-  private def getInfosFromDoc(doc: Document): Vector[ArticleInfo] = {
-    def getInfoFromListItem(li: Element): ArticleInfo = {
-      val header = li.select("h3").first
-      val url = header.select("a").attr("href")
-      val title = header.text()
-
-      val dateStr = li.select(".datestamp").text
-      val dateFormat = new java.text.SimpleDateFormat("MMM d, yyyy")
-      val date = dateFormat.parse(dateStr)
-
-      ArticleInfo(url, title, date)
-    }
-
-    val list = doc.select(".hasThumb.message_box")
-    list.map(l => getInfoFromListItem(l)).toVector
   }
 
 }
